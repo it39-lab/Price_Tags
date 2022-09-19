61ca9ad03c8644862f963793b0a4360ef098945f
Ivan Stratan <lemon@Ivans-MacBook-Air.local>
2022-09-20 00:21:44 +0300

    deleted venv

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from website import create_app
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
+app = create_app()
 
-
-def download_products_file():
-
-    file_list = drive.ListFile(
-        {'q': "'root' in parents and trashed=false"}).GetList()
-
-    for file in file_list:
-        if file['title'] == "Products":
-            file.GetContentFile(
-                filename=file['title'] + '.xlsx', mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-
-            # mimetypes = {
-            #     'application/vnd.google-apps.spreadsheet': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-            # }
-            # download_mimetype = None
-            # if file['mimeType'] in mimetypes:
-            #     download_mimetype = mimetypes[file['mimeType']]
-            # file.GetContentFile(file['title'], mimetype=download_mimetype)
-download_products_file()
+if __name__ == '__main__':
+    app.run(debug=True)
