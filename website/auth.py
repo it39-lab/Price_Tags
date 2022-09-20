845fb1dbe2261c866f8df97a2c125fa6646765f2
Lemon <btwiusearch9102@outlook.com>
2022-09-20 04:07:31 +0300

    changeeeeees

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,14 @@
-from flask import Blueprint
+from urllib import request
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html", text="Testing")
 
 
 @auth.route('/logout')
@@ -13,6 +16,24 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>sign up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firtstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to database
+            pass
+
+    return render_template("sign_up.html")
