5342b68314c969f526fde55f078569223741c3f0
Lemon <btwiusearch9102@outlook.com>
2022-09-22 18:53:18 +0300

    working sign up form

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from urllib import request
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -20,20 +20,20 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firtstName')
+        firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
-            pass
+            flash('Your email must be greater than  3 characters.', category='error')
         elif len(firstName) < 2:
-            pass
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            pass
+            flash('Password don\'t match.', category='error')
         elif len(password1) < 7:
-            pass
+            flash('Password must be at least 7 characters.', category='error')
         else:
             # add user to database
-            pass
+            flash('Account created!', category='success')
 
     return render_template("sign_up.html")
